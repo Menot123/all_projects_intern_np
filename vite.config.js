373fcfb645395c14da923d8d5bd9e5e7fa0d6a36
Menot123 <ngjyentiendat9a3@gmail.com>
2024-09-06 12:12:18 +0700

    Deloy off

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/all_projects_intern_np/',
+    // base: '/all_projects_intern_np/',
 })
